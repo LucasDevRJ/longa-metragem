9649aef1cbc4ebd67c5d04ee132f9b56cab61982
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-22 13:22:06 -0300

    Gerando métodos Getters e Setters na classe Filme

--- src/main/java/com/github/lucasdevrj/longametragem/Filme.java
+++ src/main/java/com/github/lucasdevrj/longametragem/Filme.java
@@ -4,12 +4,17 @@ import java.sql.Time;
 import java.time.Year;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity //Indica que a classe é uma entidade da JPA, ou seja, tem uma tabela no banco a representando
 @Table(name = "filme") //Nome da tabela do banco
 public class Filme {
 
+	@Id //Para mostrar o atributo id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) // para mostrar como o valor é gerado
 	private int id;
 	private String nome;
 	private int faixa;
@@ -18,4 +23,59 @@ public class Filme {
 	private Year ano;
 	private Time duracao;
 	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getNome() {
+		return nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public int getFaixa() {
+		return faixa;
+	}
+	
+	public void setFaixa(int faixa) {
+		this.faixa = faixa;
+	}
+	
+	public Categoria getCategoria() {
+		return categoria;
+	}
+	
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	public String getSinopse() {
+		return sinopse;
+	}
+	
+	public void setSinopse(String sinopse) {
+		this.sinopse = sinopse;
+	}
+	
+	public Year getAno() {
+		return ano;
+	}
+	
+	public void setAno(Year ano) {
+		this.ano = ano;
+	}
+	
+	public Time getDuracao() {
+		return duracao;
+	}
+	
+	public void setDuracao(Time duracao) {
+		this.duracao = duracao;
+	}
 }
