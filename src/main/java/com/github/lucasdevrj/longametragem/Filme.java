bd8e6b9f9bdc3902df52bd112f5505562f8033f8
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-22 11:16:14 -0300

    Alterando o banco para JDBC

--- src/main/java/com/github/lucasdevrj/longametragem/Filme.java
+++ src/main/java/com/github/lucasdevrj/longametragem/Filme.java
@@ -1,5 +1,10 @@
 package com.github.lucasdevrj.longametragem;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity //Indica que a classe é uma entidade da JPA, ou seja, tem uma tabela no banco a representando
+@Table(name = "filme") //Nome da tabela do banco
 public class Filme {
 
 	
