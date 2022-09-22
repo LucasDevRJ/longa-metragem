1e6d91740af4a59455777d582dd95eb3bf758433
Lucas Pereira de Lima <lucaspereiradelima2020@gmail.com>
2022-09-22 15:06:06 -0300

    Criação do método para cadastrar filmes

--- src/main/java/com/github/lucasdevrj/longametragem/dao/FilmeDao.java
+++ src/main/java/com/github/lucasdevrj/longametragem/dao/FilmeDao.java
@@ -1,5 +1,19 @@
 package com.github.lucasdevrj.longametragem.dao;
 
+import javax.persistence.EntityManager;
+
+import com.github.lucasdevrj.longametragem.modelo.Filme;
+//Classe para fazer a ligação com o Banco de Dados
 public class FilmeDao {
 
+	private EntityManager contrutor; //pois todos atributos terão EntityManager
+	
+	public FilmeDao(EntityManager contrutor) {
+		this.contrutor = contrutor;
+	}
+	
+	public void cadastrar(Filme filme) {
+		this.contrutor.persist(filme); //para inserir registros
+	}
+	
 }
